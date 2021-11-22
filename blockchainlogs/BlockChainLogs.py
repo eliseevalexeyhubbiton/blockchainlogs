61b16a18b7846f9a938e26008f2a0bffd344c9b8
Eliseev Alexey <whitedemonreg@gmail.com>
2021-11-22 10:27:02 +0300

    [~] Class work Good.

--- blockchainlogs/BlockChainLogs.py
+++ blockchainlogs/BlockChainLogs.py
@@ -4,14 +4,14 @@ import time
 import hashlib
 import datetime
 
-from pprint import pprint
-
 
 class BlockChainLogs:
+    _debug = False
     _error = ""
     _dir_for_logs = ""
 
-    def __init__(self, dir_logs: str = "") -> None:
+    def __init__(self, dir_logs: str = "", debug: bool = False) -> None:
+        self.debug(debug=debug)
         if bool(dir_logs) is False:
             dir_logs = os.getcwd()+"/logs"
         self.dir_logs(dir_logs=dir_logs)
@@ -27,6 +27,14 @@ class BlockChainLogs:
         :return: Error
         """
         return self._error
+
+    def debug(self, debug: bool = False) -> None:
+        """
+        GET or SET for dir_for_logs.
+        :param debug:
+        :return:
+        """
+        self._debug = debug
 
     def dir_logs(self, dir_logs: str = "") -> str:
         """
@@ -57,7 +65,12 @@ class BlockChainLogs:
         else:
             return ''
 
-    def add_block(self, data=None, ) -> None:
+    def add_block(self, data=None) -> None:
+        """
+
+        :param data:
+        :return:
+        """
         today = datetime.date.today()
         dir_year = '/' + today.strftime('%Y')
         self._check_or_create_dir(path=self._dir_for_logs + dir_year)
@@ -102,51 +115,18 @@ class BlockChainLogs:
             with open(file=path, mode="rb") as file:
                 if json.load(file)['hash'] == "0000000000000":
                     self._error = "(!!) This First Block!!"
-                    return False
+                    return True
 
             # get list files in dir where checking files
             path_dir = path[:path.rfind('/')]
             files = self._get_files_in_dir(path=path_dir)
 
             # Check file one or first in dir
-            print(f"path = {path}")
-            print(f"path_dir = {path_dir}")
             if len(files) == 1 or files.index(path[len(path_dir)+1:]) == 0:
-                file_prev = ''
-                finder = {
-                    "main_dir": path_dir,
-                    "current_dir": path_dir,
-                    "current_dir_title": path_dir[path_dir.rfind('/')+1:],
-                    "list_dirs": self._get_files_in_dir(path=path_dir[:path_dir.rfind('/')]),
-                    "previous_dir_title": '',
-                    "list_previous_dir": list(),
-                }
-                pprint(finder)
-                # find previous dir with files
-                while len(finder["list_previous_dir"]) == 0:
-                    print("One file or first in dir-----------")
-                    if finder["list_dirs"].index(finder["current_dir_title"]) == 0:
-                        print("---------------------------------")
-                        finder["current_dir"] = finder["current_dir"][:finder["current_dir"].rfind('/')]
-                        dir_previous = finder["current_dir"][finder["current_dir"].rfind('/')+1:]
-                        if dir_previous == 'logs':
-                            self._error = "(!!) Not files!!"
-                            return False
-                        finder["current_dir"] = finder["current_dir"][:finder["current_dir"].rfind('/')]
-                        list_dirs = self._get_files_in_dir(path=finder["current_dir"])
-                        finder["current_dir"] = finder["current_dir"] + '/' + list_dirs[list_dirs.index(dir_previous)-1]
-                        print(f'finder["current_dir"] = {finder["current_dir"]}')
-                    finder["list_dirs"] = self._get_files_in_dir(path=finder["current_dir"][:finder["current_dir"].rfind('/')])
-                    finder["previous_dir_title"] = finder["list_dirs"][finder["list_dirs"].index(finder["current_dir_title"])-1]
-                    finder["list_previous_dir"] = self._get_files_in_dir(
-                        path=finder["current_dir"][:finder["current_dir"].rfind('/')+1]+finder["previous_dir_title"])
-                    finder["current_dir_title"] = finder["previous_dir_title"]
-                    finder["current_dir"] = finder["current_dir"][:finder["current_dir"].rfind('/')+1] + finder["previous_dir_title"]
-                    pprint(f"finder >>>> {finder}")
-                    time.sleep(0.5)
-                return self._check_block_and_previous(block_current=path,
-                                                      block_previous=finder["current_dir"] + '/'
-                                                                     + finder["list_previous_dir"][-1])
+                file_prev = self._get_previous_file_in_tree(path=path)
+                if file_prev == '' or file_prev is None:
+                    return False
+                return self._check_block_and_previous(block_current=path, block_previous=file_prev)
             else:
                 index_check_file = files.index(path[len(path_dir)+1:])
                 file_prev = path_dir + '/' + files[index_check_file-1]
@@ -155,8 +135,29 @@ class BlockChainLogs:
             self._error = err
             return False
 
-    def check_all_blocks(self):
-        pass
+    def check_all_blocks(self) -> bool:
+        """
+
+        :return:
+        """
+        self._error = ''
+        dir_logs = self._dir_for_logs
+        print(f"{dir_logs=}")
+        list_years = self._get_dirs_in_dir(path=dir_logs)
+        for current_year in list_years:
+            list_month = self._get_dirs_in_dir(path=dir_logs + '/' + current_year)
+            for current_month in list_month:
+                list_days = self._get_dirs_in_dir(path=dir_logs + '/' + current_year + '/' + current_month)
+                for current_day in list_days:
+                    list_files = self._get_files_in_dir(
+                        path=dir_logs + '/' + current_year + '/' + current_month + '/' + current_day)
+                    if len(list_files) > 0:
+                        for current_file in list_files:
+                            result = self.check_block(file_path=dir_logs + '/' + current_year + '/' + current_month + '/' + current_day + '/' + current_file)
+                            if result is False:
+                                return False
+        return True
+
     # ###################### SERVICE___BEGIN ######################
 
     @staticmethod
@@ -199,7 +200,17 @@ class BlockChainLogs:
         :return:
         """
         files = os.listdir(path)
-        return sorted([file for file in files])
+        return sorted([file for file in files if os.path.isfile(path + '/' + file)])
+
+    @staticmethod
+    def _get_dirs_in_dir(path: str = "") -> list():
+        """
+
+        :param path:
+        :return:
+        """
+        dirs = os.listdir(path)
+        return sorted([directory for directory in dirs if os.path.isdir(path + '/' + directory)])
 
     def _check_block_and_previous(self, block_current: str = "", block_previous: str = "") -> bool:
         """
@@ -208,8 +219,9 @@ class BlockChainLogs:
         :param block_previous:
         :return:
         """
-        print(f"block_current = {block_current}")
-        print(f"block_previous = {block_previous}")
+        if self._debug is True:
+            print(f"block_current = {block_current}")
+            print(f"block_previous = {block_previous}")
         hash_prev_file = self.get_hash_file(file_name=block_previous)
         with open(file=block_current, mode="rb") as file:
             hash_prev_from_current_block = json.load(file)['hash']
@@ -217,5 +229,36 @@ class BlockChainLogs:
             return True
         else:
             return False
+
+    def _get_previous_file_in_tree(self, path: str = '') -> str:
+        self._error = ''
+        dir_logs = path
+        # block_name = dir_logs[dir_logs.rfind('/')+1:]
+        dir_logs = dir_logs[:dir_logs.rfind('/')]
+        block_day = dir_logs[dir_logs.rfind('/')+1:]
+        dir_logs = dir_logs[:dir_logs.rfind('/')]
+        block_month = dir_logs[dir_logs.rfind('/')+1:]
+        dir_logs = dir_logs[:dir_logs.rfind('/')]
+        block_year = dir_logs[dir_logs.rfind('/')+1:]
+        dir_logs = dir_logs[:dir_logs.rfind('/')]
+        list_years = self._get_dirs_in_dir(path=dir_logs)
+        list_years.reverse()
+        for current_year in list_years[list_years.index(block_year):]:
+            list_month = self._get_dirs_in_dir(path=dir_logs + '/' + current_year)
+            list_month.reverse()
+            if block_month is not None:
+                list_month = list_month[list_month.index(block_month):]
+                block_month = None
+            for current_month in list_month:
+                list_days = self._get_dirs_in_dir(path=dir_logs + '/' + current_year + '/' + current_month)
+                list_days.reverse()
+                if block_day is not None:
+                    list_days = list_days[list_days.index(block_day)+1:]
+                    block_day = None
+                for current_day in list_days:
+                    list_files = self._get_files_in_dir(path=dir_logs + '/' + current_year + '/' + current_month + '/' + current_day)
+                    if len(list_files) > 0:
+                        return dir_logs + '/' + current_year + '/' + current_month + '/' + current_day + '/' + list_files[-1]
+        return ''
 
     # ###################### SERVICE___END ######################
--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def process() -> None:
     ch_bl = os.getcwd() + "/logs/2021/11/22" + "/1637532168.197918"
     # ch_bl = os.getcwd() + "/logs/2021/11/22" + "/1637533228.935028"
     # ch_bl = os.getcwd() + "/logs/2021/11/03" + "/1637531834.840508"
-    print(block.check_block(file_path=ch_bl))
+    print(block.check_all_blocks())
+    # print(block.check_block(file_path=ch_bl))
+    # print(block._get_previous_file_in_tree(path=ch_bl))
     print(block.get_error())
 
 
